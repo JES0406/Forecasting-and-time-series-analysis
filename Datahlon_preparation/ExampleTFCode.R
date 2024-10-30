435fd70c3626e0a50405de6b55d3dcbca394c926
JES0406 <112687530+JES0406@users.noreply.github.com>
2024-10-30 23:16:49 +0100

    Finish of the training
    After a session of training my team and I got the first place finish with an astonishing first place with a RMSE of 2.60.
    The objective and best result gotten by the other class was 3.9 so the improvement was massive.

--- Datahlon_preparation/ExampleTFCode.R
+++ Datahlon_preparation/ExampleTFCode.R
@@ -1,4 +1,14 @@
+library(MLTools)
+library(fpp2)
+library(ggplot2)
+library(TSA)
+library(lmtest)  #contains coeftest function
+library(tseries) #contains adf.test function
+library(Hmisc) # for computing lagged variables
+
+
 ## Load dataset -------------------------------------------------------------------------------------------------
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 fdata <- read.table(file = "DailyPrice_Load_Wind_Spain_2019_2020_TR.csv",
                     header = T,
                     sep = ";")
@@ -17,7 +27,7 @@ x <- fdata_ts[,c("Demand","Wind")]
 autoplot(cbind(y,x), facets = TRUE)
 
 #Scale
-y.tr <- y
+y.tr <- y/10
 x.tr <- x/100
 autoplot(cbind(y.tr,x.tr), facets = TRUE)
 
@@ -26,14 +36,22 @@ autoplot(cbind(y.tr,x.tr), facets = TRUE)
 ## FIRST --------------------------------------------
 #### Fit initial FT model with large s
 #This arima function belongs to the TSA package
-TF.fit <- arima(y.tr,
-                xtransf = x.tr,
-                order=c(1,0,0),
+xlag <- lag(x, 0)
+arima.fit <- arima(y.tr,
+                xtransf = xlag,
+                order=c(1,1,1),
                 seasonal = list(order=c(1,0,0),period=7),
-                transfer = list(c(0,9),c(0,9)),
+                transfer = list(c(2,0),c(0,0)),
                 method="ML")
+# b=0, s=0, r=2
+# b=0, s=0, r=0
 
-
+summary(arima.fit)
+TF.RegressionError.plot(y, x, arima.fit, lag.max = 50)
+TF.Identification.plot(x,arima.fit) #Plot values of coefficients
+x[,1]
+ccf(y = residuals(arima.fit), x = x[,1])
+ccf(y = residuals(arima.fit), x = x[,2])
 
 ## Load dataset -----------------------------------------------------------------------------------------------
 fdataTS <- read.table(file = "DailyPrice_Load_Wind_Spain_2019_2020_TS.csv",
@@ -49,4 +67,4 @@ y_est <- TF.forecast(y.old = y.tr, #past values of the series
                     model = arima.fit, #fitted transfer function model
                     h=7) #Forecast horizon
 
-write.table(y_est, file = "PredTF.dat", col.names = FALSE, row.names = FALSE)
+write.table(y_est*10, file = "PredTF.dat", col.names = FALSE, row.names = FALSE)
